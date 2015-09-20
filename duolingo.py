a433e91dd393ed85b9c35a3fff82e5db4ca807a0
Kevin Liao <kevinliao@Atlantia.local>
2015-09-19 22:50:03 -0400

    Post request is happening, but it's not giving me what I want! >:(

--- duolingo.py
+++ duolingo.py
@@ -1,7 +1,65 @@
 # -*- coding: utf-8 -*-
 import json
 import requests
-import duolingo
+# import duolingo
+
+def post_answer(answer, element):
+
+    post_data = {
+        "session_element":{
+            "sentence": element["sentence"],
+            "solution_key": element["solution_key"],
+            "source_language":"eo",
+            "target_language_name":"English",
+            "wrong_tokens":"null",
+            "unknown_words":[      ],
+            "activity_uuid": element["activity_uuid"],
+            "specific_type": element["specific_type"],
+            "UNKNOWN":"false",
+            "generator_id": element["generator_id"],
+            "text": element["sentence"],
+            "type":"translate",
+            "new_explanation_ids":[],
+            "teaches_lexeme_ids":[
+                element["teaches_lexeme_ids"]
+            ],
+            "tokens":"null",
+            "sentence_id":element["sentence_id"],
+            "translation_key":"null",
+            "known":"true",
+            "tts_id": element["tts_id"],
+            "has_tts":"true",
+            "has_accents":"false",
+            "context":"",
+            "highlight":[
+
+      ],
+            "knowledge_components":[
+                "6bdf85dbf40e09338dd51a301633734e"
+            ],
+            "explanations":{
+
+      },
+            "target_language":"en",
+            "hovered_words":[],
+            "hovered_lexeme_ids":{
+
+      }
+        },
+        "value":answer,
+        "use_report_messages":"true",
+        "time_taken":"13508",
+        "learning_language":"eo",
+        "session_type":"practice",
+        "from_language":"en",
+        "type":"translate",
+        "solution_key": element["solution_key"]
+    }
+
+    r = requests.post("https://www.duolingo.com/session_element_solutions/practice/translate", data = post_data, headers = get_header)
+
+    print r.request.headers
+
 
 get_header = {
     "host": "www.duolingo.com",
@@ -21,68 +79,15 @@ url = "https://www.duolingo.com/sessions/588730680758?language=eo&web_speech_ena
 r = requests.get(url, headers = get_header)
 r_json = json.loads(r.text)
 
-print (r_json)
+# print (r_json)
 
 for session_element in range (0, len(r_json['session_elements'])):
     element = r_json["session_elements"][session_element]
     if element["type"] == "translate": 
         print "Please translate: " + element["sentence"]
         return_sentence = raw_input()
+        post_answer(return_sentence, element)
     else:
         print "Skipping non-translation problem."
 
 print "The quiz completed successfully."
-
-
-
-def post_answer(answer, element):
-    post_data = {
-        "session_element":{
-            "sentence": element["sentence"],
-            "solution_key": element["solution_key"],
-            "source_language":"eo",
-            "target_language_name":"English",
-            "wrong_tokens":null,
-            "unknown_words":[      ],
-            "activity_uuid": element["activity_uuid"],
-            "specific_type":"translate",
-            "unknown":false,
-            "generator_id": element["generator_id"],
-            "text": element["sentence"],
-            "type":"translate",
-            "new_explanation_ids":[],
-            "teaches_lexeme_ids":[
-                "6bdf85dbf40e09338dd51a301633734e"
-            ],
-            "tokens":null,
-            "sentence_id":"b953b7ae194a425e1a82d4acfa473f14",
-            "translation_key":null,
-            "known":true,
-            "tts_id":"b953b7ae194a425e1a82d4acfa473f14",
-            "has_tts":true,
-            "has_accents":false,
-            "context":"",
-            "highlight":[
-
-      ],
-            "knowledge_components":[
-                "6bdf85dbf40e09338dd51a301633734e"
-            ],
-            "explanations":{
-
-      },
-            "target_language":"en",
-            "hovered_words":[],
-            "hovered_lexeme_ids":{
-
-      }
-        },
-        "value":"Our nephew buys apples for us.",
-        "use_report_messages":true,
-        "time_taken":13508,
-        "learning_language":"eo",
-        "session_type":"practice",
-        "from_language":"en",
-        "type":"translate",
-        "solution_key":"b953b7ae194a425e1a82d4acfa473f14"
-    }
